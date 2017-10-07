64bdf7bc2d71905c8c3e09f25bff9ad3bae86c82
milostomsik1 <milostomsik1@gmail.com>
2017-10-07 12:02:55 +0200

    fixed tab spacing

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,12 @@ import Post from '../controllers/posts'
 const posts = express()
 
 posts.route('/')
-      .post(Post.createPost)
-      .get(Post.getPosts)
+     .post(Post.createPost)
+     .get(Post.getPosts)
 
 posts.route('/:id')
-      .get(Post.getPost)
-      .put(Post.updatePost)
-      .delete(Post.deletePost)
+     .get(Post.getPost)
+     .put(Post.updatePost)
+     .delete(Post.deletePost)
 
 export default posts
